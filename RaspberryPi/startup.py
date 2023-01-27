a727c6af922cdb8f5ac3c527ef04dd8e8a24ee81
philip <85298560+philipstr01@users.noreply.github.com>
2023-01-27 12:23:17 +0100

    Delete RaspberryPi directory

--- RaspberryPi/startup.py
+++
@@ -1,5 +0,0 @@
-import git
-
-repo = git.Repo.clone_from('git@gitlab.com:philipstr01/sphere-ixd/RaspberryPi/raspy',
-                           '/home/philip/Documents',
-                           branch='master')
