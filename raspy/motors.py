c5c74cf737fd854f913c5a712de886b5b216f461
philip <85298560+philipstr01@users.noreply.github.com>
2023-01-30 13:11:54 +0100

    Update motors.py

--- raspy/motors.py
+++ raspy/motors.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 
+"""
 # anpassen, falls andere Sequenz
 StepCount = 8
 Seq = list(range(0, StepCount))
@@ -45,3 +46,74 @@ class motor:
             for j in reversed(range(StepCount)):
                 self.setStep(Seq[j][0], Seq[j][1], Seq[j][2], Seq[j][3])
                 time.sleep(delay)
+"""
+
+class motor:
+    def __init__(self,A,B,C,D):
+        GPIO.setmode(GPIO.BOARD)
+        # PIN-Assignment
+        self.A=18
+        self.B=23
+        self.C=24
+        self.D=25
+        time = 0.001
+        # defining the PINs
+        GPIO.setup(self.A,GPIO.OUT)
+        GPIO.setup(self.B,GPIO.OUT)
+        GPIO.setup(self.C,GPIO.OUT)
+        GPIO.setup(self.D,GPIO.OUT)
+        GPIO.output(self.A, False)
+        GPIO.output(self.B, False)
+        GPIO.output(self.C, False)
+        GPIO.output(self.D, False)
+    # driving the motor
+    def Step1(self):
+        GPIO.output(self.D, True)
+        sleep (time)
+        GPIO.output(self.D, False)
+    def Step2(self):
+        GPIO.output(self.D, True)
+        GPIO.output(self.C, True)
+        sleep (time)
+        GPIO.output(self.D, False)
+        GPIO.output(self.C, False)
+    def Step3(self):
+        GPIO.output(self.C, True)
+        sleep (time)
+        GPIO.output(self.C, False)
+    def Step4(self):
+        GPIO.output(self.B, True)
+        GPIO.output(self.C, True)
+        sleep (time)
+        GPIO.output(self.B, False)
+        GPIO.output(self.C, False)
+    def Step5(self):
+        GPIO.output(self.B, True)
+        sleep (time)
+        GPIO.output(self.B, False)
+    def Step6(self):
+        GPIO.output(self.A, True)
+        GPIO.output(self.B, True)
+        sleep (time)
+        GPIO.output(self.A, False)
+        GPIO.output(self.B, False)
+    def Step7(self):
+        GPIO.output(self.A, True)
+        sleep (time)
+        GPIO.output(self.A, False)
+    def Step8(self):
+        GPIO.output(self.D, True)
+        GPIO.output(self.A, True)
+        sleep (time)
+        GPIO.output(self.D, False)
+        GPIO.output(self.A, False)
+    def forward(self,j):
+        for i in range (512):
+            self.Step1()
+            self.Step2()
+            self.Step3()
+            self.Step4()
+            self.Step5()
+            self.Step6()
+            self.Step7()
+            self.Step8()
