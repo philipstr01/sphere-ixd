f5253667a229ed383439051f5e9d0639c6089f2d
philip <85298560+philipstr01@users.noreply.github.com>
2023-02-03 22:26:43 +0100

    Update main.py

--- raspy/main.py
+++ raspy/main.py
@@ -7,10 +7,13 @@ import time
 #df = base_functions.getsurveyDataframe()
 #mysurvey = base_functions.getSurveyObject()
 
-#Motor Test script
-
-motor1 = motors.motor(13,15,16,18)
-
+#Motor Test script,
+GPIO.setmode(GPIO.BCM)
+motor1 = motors.motor(2,3,4,14)
+motor2 = motors.motor(15,17,18,27)
+motor3 = motors.motor(22,23,24,10)
+motor4 = motors.motor(9,11,8,7)
+motor5 = motors.motor(5,6,12,13)
 motor1.forward(5)
 #motor1.backward(1)
 GPIO.cleanup()
