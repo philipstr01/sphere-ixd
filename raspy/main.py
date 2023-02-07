6f1dff8a4abd2f99694b5c3949e4ae253fadfedd
philip <85298560+philipstr01@users.noreply.github.com>
2023-02-07 20:21:51 +0100

    Update main.py

--- raspy/main.py
+++ raspy/main.py
@@ -2,7 +2,7 @@ import base_functions
 import motors
 import RPi.GPIO as GPIO
 import time
-#import leds
+import leds
 
 #base_functions.download_write_responses()
 #df = base_functions.getsurveyDataframe()
@@ -17,6 +17,7 @@ motor3 = motors.motor(22,23,24,10)
 motor4 = motors.motor(9,11,8,7)
 motor5 = motors.motor(5,6,12,13)
 contr = motors.motorcontroller(motors.motorarray({motor1:False,motor2:False,motor3:True,motor4:True,motor5:True}))
-contr.heights = [0,0,0,0,0]
-contr.changeHeights([3.5,3,2.5,2,1.5])
+contr.changeHeights([3,3,2,2,1])
+contr.changeHeights([0,0,1,0,0])
+contr.setHeights([0,0,0,0,0])
 GPIO.cleanup()
