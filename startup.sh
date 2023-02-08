b8521094c512f03205b7819806e8c956344231b3
philip <85298560+philipstr01@users.noreply.github.com>
2023-02-08 12:13:19 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,8 +4,6 @@ cd /home/pi/Documents/sphere-ixd
 git reset --hard
 git pull origin main
 
-echo waiting 10s
-sleep 10
 echo executing python main
 cd /home/pi/Documents/sphere-ixd/raspy/
 python3 main.py &
