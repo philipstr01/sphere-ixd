870a620f023ffcffe9f2edd193aaf031620adfe2
philip <85298560+philipstr01@users.noreply.github.com>
2023-02-09 17:32:44 +0100

    Update main.py

--- raspy/main.py
+++ raspy/main.py
@@ -2,6 +2,8 @@ import base_functions
 import motors
 import RPi.GPIO as GPIO
 import time
+import neopixel
+import board
 #import leds
 
 #base_functions.download_write_responses()
@@ -11,6 +13,7 @@ import time
 #Motor Test script,
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
+"""
 motor1 = motors.motor(2,3,4,14)
 motor2 = motors.motor(15,17,18,27)
 motor3 = motors.motor(22,23,24,10)
@@ -19,4 +22,8 @@ motor5 = motors.motor(5,6,12,13)
 contr = motors.motorcontroller(motors.motorarray([motor1,motor2,motor3,motor4,motor5]))
 contr.jank()
 contr.setHeights([2,2,2,2,2])
+"""
+pixels = neopixel.NeoPixel(board.D19,80)
+pixel[0] = (255,0,0)
+pixel[79] = (0,0,255)
 GPIO.cleanup()
