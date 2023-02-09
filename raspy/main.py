3dcaccd89b4d7fadac5c272f5a1499c2f565caeb
philip <85298560+philipstr01@users.noreply.github.com>
2023-02-09 17:44:58 +0100

    Update main.py

--- raspy/main.py
+++ raspy/main.py
@@ -15,7 +15,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 """
 motor1 = motors.motor(2,3,4,14)
-motor2 = motors.motor(15,17,18,27)
+motor2 = motors.motor(15,17,19,27)
 motor3 = motors.motor(22,23,24,10)
 motor4 = motors.motor(9,11,8,7)
 motor5 = motors.motor(5,6,12,13)
@@ -23,7 +23,7 @@ contr = motors.motorcontroller(motors.motorarray([motor1,motor2,motor3,motor4,mo
 contr.jank()
 contr.setHeights([2,2,2,2,2])
 """
-pixels = neopixel.NeoPixel(board.D35,80)
+pixels = neopixel.NeoPixel(board.D18,80)
 pixels[0] = (255,0,0)
 pixels[79] = (0,0,255)
 GPIO.cleanup()
