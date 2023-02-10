f94f9c8710ca1790da07b55bc5945124994e068a
philip <85298560+philipstr01@users.noreply.github.com>
2023-02-10 16:31:00 +0100

    Update leds.py

--- raspy/leds.py
+++ raspy/leds.py
@@ -13,16 +13,16 @@ class ledcontroller:
     def setPixelRow(self,color,row):
         if row > 16:
             return
-        elif row > 14:
+        elif row > 13:
             self.pixels[7+14+row] = color
             self.pixels[7-1+14+(2*17)-row] = color
-        elif row > 9:
+        elif row > 8:
             #self.pixels[7-1+14+row] = color
             #self.pixels[7+14+(2*17)-row] = color
             #self.pixels[7-1+row] = color
             #self.pixels[7+2*14+(2*17)-row] = color
             pass
-        elif row > 0:
+        elif row >= 0:
             #self.pixels[7-1+14+row] = color
             #self.pixels[7+14+(2*17)-row] = color
             pass
