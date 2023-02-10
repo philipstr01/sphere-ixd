6dc1dd172e27f1d514acc1637ce927b7ed0f078a
philip <85298560+philipstr01@users.noreply.github.com>
2023-02-10 16:07:48 +0100

    Update main.py

--- raspy/main.py
+++ raspy/main.py
@@ -25,12 +25,17 @@ GPIO.setwarnings(False)
 
 led = leds.ledcontroller()
 led.pixels.fill((0,0,0))
+"""
 n = 0
 while n<17:
     led.setPixelRow((255,0,n*10),n)
     n += 1
     print(n)
     time.sleep(0.5)
-    
+"""
+while True:
+    if input("Continue? ") == "no"
+        break
+    led.pixels[int(input("Turn on on LED Number: "))] = (255,255,255)
 led.pixels.fill((0,0,0))
 GPIO.cleanup()
