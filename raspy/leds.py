9b809fc2fda5140f186eac4732130ab3a7e0585e
philip <85298560+philipstr01@users.noreply.github.com>
2023-02-10 15:51:26 +0100

    Add files via upload

---
+++ raspy/leds.py
@@ -0,0 +1,26 @@
+import os 
+import neopixel
+import board
+
+class ledcontroller:
+    def __init__(self,boardpin=board.D18,n_leds=80)
+        self.pixels = neopixel.NeoPixel(boardpin,n_leds)
+        self.colors = [(0,0,0)]*5
+
+    def setPixelRow(color,row):
+        if row > 17:
+            return
+        elif row > 14:
+            self.pixels[9+14+1+row] = color
+            self.pixels[9+14+2*17-row] = color
+        elif row > 9:
+            self.pixels[9+14+1+row] = color
+            self.pixels[9+14+2*17-row] = color
+        elif row > 0:
+
+        else:
+            return
+        # 9 14 17
+
+
+
