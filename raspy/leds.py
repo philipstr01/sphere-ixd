d534a61db4d90e2568da61cd2c58daa0ef703cef
philip <85298560+philipstr01@users.noreply.github.com>
2023-02-11 12:10:07 +0100

    Update leds.py

--- raspy/leds.py
+++ raspy/leds.py
@@ -1,11 +1,19 @@
 import os 
 import neopixel
 import board
+import time
+
+def t_add(t1,t2):
+    return tuple(map(sum, zip(t1, t2)))
+
+def t_mul(s,t):
+    return tuple([s*x for x in t])
+
 
 class ledcontroller:
     def __init__(self,boardpin=board.D18,n_leds=80):
         self.pixels = neopixel.NeoPixel(boardpin,n_leds)
-        self.colors = [(0,0,0)]*5
+        self.colors = self.colors = [(174,126,235),(152,227,245),(120,222,104),(245,210,59),(224,114,88)]
 
     #LED Row Distribution
     # 7 14 17 17 14 9        
@@ -40,6 +48,37 @@ class ledcontroller:
             pass
         else:
             return
+        
+    def colorGradient(self,t):
+        if t > 1:
+            c = self.colors[4]
+        if t > 3/4:
+            c = t_add(t_mul(4*(t-3/4),self.colors[4]),t_mul((1-4*(t-3/4)),self.colors[3]))
+        elif t > 2/4:
+            c = t_add(t_mul(4*(t-2/4),self.colors[3]),t_mul((1-4*(t-2/4)),self.colors[2]))
+        elif t > 1/5:
+            c = t_add(t_mul(4*(t-1/4),self.colors[2]),t_mul((1-4*(t-1/4)),self.colors[1]))
+        elif t > 0/5:
+            c = t_add(t_mul(4*t,self.colors[1]),t_mul((1-4*t),self.colors[0]))
+        else:
+            c = self.colors[0]
+        return c
+
+    def colorRows(self,t):
+        n = 0
+        b = 1/32
+        while n<17:
+            self.setPixelRow(self.colorGradient(t+b*n),n)
+            n += 1
+
+    def colorCycle(self,a,b):
+        x = a
+        while x <= b:
+            x+=1e-3
+            self.colorRows(x)
+            time.sleep(1e-3)
+
+        
         
 
 
