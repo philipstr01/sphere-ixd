bd2f05a45ce284a0a330a6b619162b17de0fa971
philip <85298560+philipstr01@users.noreply.github.com>
2023-02-15 13:49:15 +0100

    Update main.py

--- raspy/main.py
+++ raspy/main.py
@@ -10,12 +10,12 @@ from datetime import datetime, timedelta
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-motor1 = motors.motor(2,3,4,14)
-motor2 = motors.motor(15,17,19,27)
-motor3 = motors.motor(22,23,24,10)
-motor4 = motors.motor(9,11,8,7)
-motor5 = motors.motor(5,6,12,13)
-marray = motors.motorarray([motor1,motor2,motor3,motor4,motor5])   
+motor4 = motors.motor(2,3,4,14)
+motor3 = motors.motor(15,17,19,27)
+motor1 = motors.motor(22,23,24,10)
+motor2 = motors.motor(9,11,8,7)
+motor0 = motors.motor(5,6,12,13)
+marray = motors.motorarray([motor0,motor1,motor2,motor3,motor4])   
 contr = motors.motorcontroller(marray)
 
 led = leds.ledcontroller()
